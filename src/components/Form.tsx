188ab8b62d54bbc04771fc73e39732d68c511516
Aditya Phasu <adityaphasu919@gmail.com>
2026-01-10 23:58:03 +0530

    update Form layout with navigation buttons for steps

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { Step1 } from "./Step1";
+import { Step2 } from "./Step2";
 
 export const Form = ({
   step,
@@ -7,11 +8,39 @@ export const Form = ({
   step: number;
   setStep: React.Dispatch<React.SetStateAction<number>>;
 }) => {
+  const handleNext = (e: React.FormEvent) => {
+    e.preventDefault();
+    setStep((prev) => prev + 1);
+  };
+
   return (
-    <form className="pt-1.5 flex-1 flex flex-col justify-between h-full">
-      <div className="bg-white rounded-xl shadow-xl -mt-20 mx-4">{step === 1 && <Step1 />}</div>
-      <div className="bg-white flex justify-end px-4 pb-3 pt-4">
-        <button className="text-white bg-marine-blue px-3.75 py-2.25 rounded text-[15px]">
+    <form className="pt-1.5 flex-1 flex flex-col justify-between h-full -mt-20">
+      {step === 1 && (
+        <div className="bg-white rounded-xl shadow-xl mx-4">
+          {" "}
+          <Step1 />
+        </div>
+      )}
+      {step === 2 && (
+        <div className="bg-white rounded-xl shadow-xl mx-4">
+          <Step2 />
+        </div>
+      )}
+      <div
+        className={`bg-white flex px-4 pb-3 pt-4 text-[15px] ${
+          step > 1 ? "justify-between" : "justify-end"
+        }`}>
+        {step > 1 && (
+          <button
+            onClick={() => setStep((prev) => prev - 1)}
+            className="cursor-pointer text-cool-gray">
+            Go Back
+          </button>
+        )}
+        <button
+          onClick={handleNext}
+          type="submit"
+          className="text-white bg-marine-blue px-3.75 py-2.25 rounded  cursor-pointer">
           Next Step
         </button>
       </div>
