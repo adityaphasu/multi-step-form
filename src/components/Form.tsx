6a32aaec5dbcd452f41bb5f19b8bc57b2c566a0e
Aditya Phasu <adityaphasu919@gmail.com>
2026-01-18 21:00:41 +0530

    add confirmation step and loading state in form submission

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,6 +5,7 @@ import { Step1 } from "./Step1";
 import { Step2 } from "./Step2";
 import { Step3 } from "./Step3";
 import { Step4 } from "./Step4";
+import { Confirm } from "./Confirm";
 
 export const Form = ({
   step,
@@ -26,9 +27,22 @@ export const Form = ({
       largerStorage: { checked: true },
     },
   });
+  const [isConfirmed, setIsConfirmed] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleNext = (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (step === 4) {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        setIsConfirmed(true);
+      }, 3000);
+
+      return;
+    }
+
     setStep((s) => Math.min(s + 1, 4));
   };
 
@@ -43,29 +57,41 @@ export const Form = ({
     <form
       onSubmit={handleNext}
       className="pt-1.5 flex flex-1 flex-col justify-between h-full -mt-20 gap-10">
-      <div className="bg-white rounded-xl shadow-xl mx-4">{steps[step - 1]}</div>
-      <div
-        className={`bg-white flex px-4 pb-3 pt-4 text-[15px] ${
-          step > 1 ? "justify-between" : "justify-end"
-        }`}>
-        {step > 1 && (
-          <button
-            type="button"
-            onClick={() => setStep((s) => Math.max(s - 1, 1))}
-            className="cursor-pointer text-cool-gray transition-all hover:text-marine-blue font-medium">
-            Go Back
-          </button>
-        )}
-        <button
-          type="submit"
-          className={`text-white py-2.25 rounded  cursor-pointer transition-colors ${
-            step === 4
-              ? "px-5.25 bg-purplish-blue hover:bg-purplish-blue/80"
-              : "bg-marine-blue px-3.75 hover:bg-marine-blue/80"
-          }`}>
-          {step === 4 ? "Confirm" : "Next Step"}
-        </button>
+      <div className="bg-white rounded-xl shadow-xl mx-4">
+        {isConfirmed ? <Confirm /> : steps[step - 1]}
       </div>
+      {!isConfirmed && (
+        <div
+          className={`bg-white flex px-4 pb-3 pt-4 text-[15px] ${
+            step > 1 ? "justify-between" : "justify-end"
+          }`}>
+          {step > 1 && (
+            <button
+              type="button"
+              onClick={() => setStep((s) => Math.max(s - 1, 1))}
+              className="cursor-pointer text-cool-gray transition-all hover:text-marine-blue font-medium">
+              Go Back
+            </button>
+          )}
+          <button
+            type="submit"
+            className={`text-white py-2.25 rounded  cursor-pointer transition-colors ${
+              step === 4
+                ? "px-5.25 bg-purplish-blue hover:bg-purplish-blue/80"
+                : "bg-marine-blue px-3.75 hover:bg-marine-blue/80"
+            }`}>
+            {step === 4 ? (
+              loading ? (
+                <span className="block size-5.5 border-2 border-white rounded-full border-t-transparent animate-spin"></span>
+              ) : (
+                "Confirm"
+              )
+            ) : (
+              "Next Step"
+            )}
+          </button>
+        </div>
+      )}
     </form>
   );
 };
