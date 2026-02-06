015581bba565fba8dc921dffcc6d128d9feadc05
Aditya Phasu <adityaphasu919@gmail.com>
2026-02-06 21:10:47 +0530

    add h1 tag and use rem for media query for a11y

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <main className="min-h-screen flex flex-col lg:grid lg:grid-cols-[17.15rem_1fr] lg:bg-white lg:justify-center lg:mx-auto lg:min-h-auto lg:rounded-xl lg:shadow-xl lg:px-4 lg:py-4 lg:min-w-235 lg:gap-19">
+      <h1 className="sr-only">Multi-Step Form</h1>
       <div className="background-image w-full h-43 flex items-top justify-center py-8 px-2 lg:w-auto lg:rounded-lg lg:py-9.5 lg:h-auto lg:justify-start lg:px-8">
         <StepIndicators step={step} />
       </div>
